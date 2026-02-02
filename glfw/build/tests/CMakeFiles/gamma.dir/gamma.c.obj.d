024e20887afc54204ba9868126d7c305b0dcd98e
AdamYChe <adamyc2018@gmail.com>
2026-02-02 18:38:17 -0500

    Initial commit; environment setup

---
+++ glfw/build/tests/CMakeFiles/gamma.dir/gamma.c.obj.d
@@ -0,0 +1,35 @@
+tests/CMakeFiles/gamma.dir/gamma.c.obj: \
+ C:\Users\adamy\CLionProjects\graphicsTesting\glfw\tests\gamma.c \
+ C:/Users/adamy/CLionProjects/graphicsTesting/glfw/deps/glad/gl.h \
+ C:/mingw64/lib/gcc/x86_64-w64-mingw32/7.3.0/include/stdint.h \
+ C:/mingw64/x86_64-w64-mingw32/include/stdint.h \
+ C:/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_directx.h \
+ C:/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/mingw64/lib/gcc/x86_64-w64-mingw32/7.3.0/include/stddef.h \
+ C:/mingw64/x86_64-w64-mingw32/include/stddef.h \
+ C:/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw_print_push.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw_print_pop.h \
+ C:/mingw64/x86_64-w64-mingw32/include/stdlib.h \
+ C:/mingw64/lib/gcc/x86_64-w64-mingw32/7.3.0/include-fixed/limits.h \
+ C:/mingw64/lib/gcc/x86_64-w64-mingw32/7.3.0/include-fixed/syslimits.h \
+ C:/mingw64/x86_64-w64-mingw32/include/limits.h \
+ C:/mingw64/x86_64-w64-mingw32/include/sec_api/stdlib_s.h \
+ C:/mingw64/x86_64-w64-mingw32/include/malloc.h \
+ C:/mingw64/x86_64-w64-mingw32/include/string.h \
+ C:/mingw64/x86_64-w64-mingw32/include/sec_api/string_s.h \
+ C:/Users/adamy/CLionProjects/graphicsTesting/glfw/include/GLFW/glfw3.h \
+ C:/Users/adamy/CLionProjects/graphicsTesting/glfw/deps/nuklear.h \
+ C:/mingw64/lib/gcc/x86_64-w64-mingw32/7.3.0/include/stdarg.h \
+ C:/mingw64/x86_64-w64-mingw32/include/stdarg.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw_stdarg.h \
+ C:/mingw64/x86_64-w64-mingw32/include/assert.h \
+ C:/Users/adamy/CLionProjects/graphicsTesting/glfw/deps/nuklear_glfw_gl2.h
