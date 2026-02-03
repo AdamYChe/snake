45b83d5507f5a40fb98a63b4d0b165152291aa7a
AdamYChe <adamyc2018@gmail.com>
2026-02-02 19:35:27 -0500

    Create initial window.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,58 @@
 #include <GLFW/glfw3.h>
 #include "glm/glm.hpp"
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+#if defined(_WIN32) || defined(_WIN64)
+#include <Windows.h>
+    int SCREEN_WIDTH = GetSystemMetrics(SM_CXFULLSCREEN);
+    int SCREEN_HEIGHT = GetSystemMetrics(SM_CYFULLSCREEN);
+#else
+    int SCREEN_WIDTH = 1600;
+    int SCREEN_HEIGHT = 1200;
+#endif
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "3D Snake", nullptr, nullptr);
+    if(!window) {
+        std::cout << "Failed to initialize GLFWwindow." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD." << std::endl;
+        return -1;
+    }
+
+    while(!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
